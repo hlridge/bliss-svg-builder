4639cbd8a0263b8eab4edafed7538b15f8d83498
Hannes Ljusås <hsls@hotmail.com>
2025-03-10 13:50:11 +0100

    create three different build files (#3)

--- vite.config.js
+++ vite.config.js
@@ -2,25 +2,26 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig(({ command }) => {
-  const config = {
-    publicDir: 'examples',
-  };
-
-  if (command === 'build') {
-    return {
-      build: {
-        minify: false,
-        lib: {
-          entry: resolve(__dirname, 'src/index.js'),
-          name: 'BlissSVGBuilder',
-          formats: ['es', 'umd'],
-        },
-        rollupOptions: {
-          //...
+  return {
+    build: {
+      minify: false,
+      lib: {
+        entry: resolve(__dirname, 'src/index.js'),
+        name: 'BlissSVGBuilder',
+        formats: ['es', 'umd', 'cjs'],
+        fileName: (format) => {
+          switch (format) {
+            case 'es':
+              return 'bliss-svg-builder.esm.js'; // For ESM (both browser and Node.js)
+            case 'umd':
+              return 'bliss-svg-builder.umd.js'; // For browsers using UMD
+            case 'cjs':
+              return 'bliss-svg-builder.cjs'; // For Node.js CommonJS
+            default:
+              throw new Error(`Unsupported format: ${format}`);
+          }
         },
       },
-    };
-  }
-
-  return config;
+    },
+  };
 });
