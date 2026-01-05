0a4cdbb659ae49e320a970e87fae99aa0b7ea277
Hannes Ljusås <hsls@hotmail.com>
2026-01-05 17:40:26 +0100

    docs: update examples to use B313 character

--- examples/node.cjs
+++ examples/node.cjs
@@ -1,5 +1,5 @@
 // Example Node CommonJS
 const { BlissSVGBuilder } = require('../dist/bliss-svg-builder.cjs');
 
-const builder = new BlissSVGBuilder("H:0,8");
+const builder = new BlissSVGBuilder("B313");
 console.log(builder.svgCode);
