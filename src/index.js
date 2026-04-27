1da74d9d4e329161f28cfab16762eae032d7b711
Hannes Ljusås <hsls@hotmail.com>
2026-04-27 07:04:07 +0200

    feat: hoist LIB_VERSION as static, add default export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import { BlissSVGBuilder } from './lib/bliss-svg-builder.js'
 import { LIB_VERSION } from './lib/bliss-constants.js'
 
+// Hoist LIB_VERSION onto the class so IIFE consumers, where
+// `window.BlissSVGBuilder` is the class itself, can read the
+// version as `BlissSVGBuilder.LIB_VERSION`.
+BlissSVGBuilder.LIB_VERSION = LIB_VERSION;
+
 export { BlissSVGBuilder, LIB_VERSION };
+export default BlissSVGBuilder;
